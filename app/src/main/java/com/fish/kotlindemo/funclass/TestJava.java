38444d99abd58c6ff60ee9cd7fba5b912f34d8aa
fishforest <409652578@qqo.com>
2022-05-17 00:52:29 +0800

    初步分解函数类型

--- app/src/main/java/com/fish/kotlindemo/funclass/TestJava.java
+++ app/src/main/java/com/fish/kotlindemo/funclass/TestJava.java
@@ -10,5 +10,22 @@ public class TestJava {
                 return null;
             }
         });
+
+//        ClassFunKt.getTest1().invoke()
+    }
+
+
+    private void testStudent(HandleStudent handleStudent) {
+        float score = 0;
+        if (handleStudent != null) {
+            score = handleStudent.getScore("fish", 18);
+        }
+        System.out.println("score:" + score);
+    }
+
+
+    interface HandleStudent {
+        //传入学生的姓名、年龄，返回学生的分数
+        float getScore(String name, int age);
     }
 }
--- app/src/main/java/com/fish/kotlindemo/funclass/UpFun.kt
+++ app/src/main/java/com/fish/kotlindemo/funclass/UpFun.kt
@@ -1,1 +1,15 @@
 package com.fish.kotlindemo.funclass
+
+fun upFun1(name: String, age: Int): Float {
+    return 88f
+}
+
+//testUpFun1 接收的参数为函数类型：(String, Int)->String
+fun testUpFun1(getScore : (String, Int)->Float) {
+    var score = getScore("fish", 18)
+    println("student score:$score")
+}
+
+fun main(args: Array<String>) {
+    testUpFun1(::upFun1)
+}
